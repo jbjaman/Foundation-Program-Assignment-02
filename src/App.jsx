9408026a8a1f1878050fc20b58b276c5348abd52
jbjzeehad <ilamta.97@gmail.com>
2026-09-16 23:24:26 +0600

    Add initial project setup with React, Tailwind CSS, and SVG icons
    - Created main application file (App.jsx) with a header.
    - Added global CSS file (index.css) and imported Tailwind CSS.
    - Set up entry point (main.jsx) for React application.
    - Configured Vite with React and Tailwind CSS plugins.
    - Added SVG icons for Bluesky, Discord, Documentation, GitHub, Social, and X.
    - Included hero image and React logo in assets.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl flex items-center justify-center mt-56">
+        CHOBIKHOJ BD
+      </h1>
+    </>
+  );
+}
+
+export default App;
