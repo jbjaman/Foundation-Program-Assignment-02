230f20f11758fbb6557e863ad77e76c38dc8f3fc
jbjzeehad <ilamta.97@gmail.com>
2026-09-18 02:42:27 +0600

    Enhance Movies and Card components with lucide-react icons for search, rating, and year display

--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -1,3 +1,4 @@
+import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
 import { fetchMovies } from "../api/chobikhoj";
 import Card from "../components/Card";
@@ -37,8 +38,11 @@ const Movies = () => {
     <div className="bg-linear-to-r from-slate-900 via-slate-400 to-slate-900 p-4">
       <div className="sticky top-20 z-30 mb-5">
         <div className="flex items-center gap-3 rounded-full  border-4 border-slate-600 px-5 py-2 backdrop-blur-lg">
-          <span aria-hidden="true" className="text-slate-50">
-            Search
+          <span
+            aria-hidden="true"
+            className="text-slate-50 flex items-center gap-1"
+          >
+            <Search color="white" strokeWidth={1} size={17} /> Search
           </span>
           <input
             type="text"
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import { CalendarDays, Film, Star } from "lucide-react";
+
 const Card = ({ name, poster, rating, year }) => {
   return (
     <div className="relative rounded-lg ">
@@ -8,10 +10,18 @@ const Card = ({ name, poster, rating, year }) => {
       />
 
       <div className=" absolute bottom-0 bg-slate-900/90 w-full rounded-b-lg text-slate-50 p-2 ">
-        <h3 className=" text-xl font-bold mb-3">{name}</h3>
+        <h3 className=" text-xl font-bold mb-3 flex items-center gap-1">
+          <Film color="white" strokeWidth={1} size={18} /> {name}
+        </h3>
         <div className="flex  items-center justify-between ">
-          <span>⭐ {rating}</span>
-          <span>📅 {year}</span>
+          <span className="flex items-center gap-1">
+            {" "}
+            <Star color="orange" strokeWidth={1} size={16} /> {rating}
+          </span>
+          <span className="flex gap-1 items-center">
+            {" "}
+            <CalendarDays color="orange" strokeWidth={1} size={16} /> {year}
+          </span>
         </div>
 
         <button className="mt-3 hover:bg-amber-400 text-sm hover:text-slate-800 px-4 py-2 rounded-md font-extrabold cursor-pointer w-full bg-transparent border-amber-400 border text-slate-50">
