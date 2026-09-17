d40af283c0fd8f70f8627e881eb98c59aaefd84b
jbjzeehad <ilamta.97@gmail.com>
2026-09-17 18:29:54 +0600

    Add Footer and Navbar components with updated styles and logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 const Footer = () => {
   return (
     <>
-      <footer className="bg-[#0B0B0F] text-white h-18">CHOBIKHOJ BD</footer>
+      <footer className="bg-[#0B0B0F] ">
+        <div className="mx-auto  flex py-3 gap-3 px-5 items-center justify-center">
+          <img src="/chobikhoj.png" alt="" className="w-24 h-auto" />
+          <p className="text-sm text-slate-400">© 2026 CHOBIKHOJ BD.</p>
+        </div>
+      </footer>
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
 const Navbar = () => {
   return (
     <>
-      <div className="bg-amber-200 flex items-center justify-between p-3 z-10 h-12">
-        <h1>CHOBIKHOJ BD</h1>
-        <a href="">Movies</a>
+      <div className="sticky top-0 mx-auto bg- flex items-center justify-between py-2  px-7 z-10    bg-linear-to-r from-amber-900 via-amber-500 to-blue-800 font-bold  text-slate-50 ">
+        <img
+          src="/chobikhoj.png"
+          alt=""
+          className="w-36 h-auto brightness-200 contrast-125"
+        />
+        <div className=" flex items-center justify-center gap-2 ">
+          <a
+            href="/"
+            className="border border-slate-50/40 px-2 py-1 rounded-md hover:bg-amber-500 hover:border-blue-800 transition-all duration-300"
+          >
+            Home
+          </a>
+          <a
+            href=""
+            className="border border-slate-50/40 px-2 py-1 rounded-md hover:bg-amber-500 hover:border-blue-800 transition-all duration-300"
+          >
+            Movies
+          </a>
+        </div>
       </div>
     </>
   );
