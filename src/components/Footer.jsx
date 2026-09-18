319178dd79a79ef94fcee52564c23aacc62e3222
jbjzeehad <ilamta.97@gmail.com>
2026-09-18 21:14:29 +0600

    Add react-icons for social media links in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,6 @@
+import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 const Footer = () => {
   return (
     <>
@@ -5,6 +8,12 @@ const Footer = () => {
         <div className="mx-auto  flex py-3 gap-3 px-5 items-center justify-center">
           <img src="/chobikhoj.png" alt="" className="w-24 h-auto" />
           <p className="text-sm text-slate-400">© 2026 CHOBIKHOJ BD.</p>
+        </div>
+        <div className="border-amber-50 text-slate-50 flex items-center justify-center gap-3 pb-3 text-sm">
+          <FaFacebook />
+          <FaInstagram />
+          <FaGithub />
+          <FaXTwitter />
         </div>
       </footer>
     </>
